5475650ee4219758594ae7dab21b4f81a137106c
jansen-jra_dev <jansen.lloyd.macabangun@jrati.com>
2026-05-16 14:10:44 +0800

    fix: angular routing

--- login_page/src/app/app.routes.ts
+++ login_page/src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login-component/login-component';
 
 export const routes: Routes = [
-    { path: '' , component: LoginComponent}
+    { path: '' , component: LoginComponent},
+    { path: '**', redirectTo: '', pathMatch: 'full'}
 ];
