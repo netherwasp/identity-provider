bde2ce2349ba546d3839be6a7125da09efb08356
jansen-jra_dev <jansen.lloyd.macabangun@jrati.com>
2026-08-31 14:35:23 +0800

    feat: updated login page

--- frontend/auth_service/src/app/component/page/login/login.ts
+++ frontend/auth_service/src/app/component/page/login/login.ts
@@ -32,8 +32,8 @@ export class Login {
     private fb: FormBuilder,
   ) {
     this.loginForm = this.fb.group({
-      username: ['', [Validators.required, Validators.minLength(3)]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
     });
   }
 
